f35ae20a13b53c989791229ffbf810c1946410db
Hugo Melo <hugo@yellowmaverick.com>
2024-01-31 15:14:01 -0300

    Add Tag Cloud and fix alova

---
+++ src/components/TagCloud.vue
@@ -0,0 +1,33 @@
+<script>
+import { alove } from '../js/alova.js';
+import VueWordCloud from 'vuewordcloud';
+
+export default{
+  data () {
+    return {
+      data: null
+    }
+  },
+  components: { VueWordCloud },
+  mounted() {
+    alove.Get('/v1/queries/1/run').then(r => r.clone().json() ).then(async r => {
+      this.data = r.data.slice(1)
+    })
+  },
+  methods: {
+    formatData(row) {
+      if (!row) return
+      return row.map(r => [r.text, r.weight])
+    }
+  },
+}
+</script>
+
+<template>
+  <div v-for="product in data" :key="product" v-if='data' class='mt-4'>
+    <h5>{{ product[1] }} - <span style='color: green'>{{ $t('word_cloud.positive_words') }}</span></h5>
+    <VueWordCloud class='w-100' style="height: 480px" :words="formatData(product[2])"></VueWordCloud>
+    <h5>{{ product[1] }} - <span style='color: red'>{{ $t('word_cloud.negative_words') }}</span></h5>
+    <VueWordCloud class='w-100' style="height: 480px" :words="formatData(product[3])"></VueWordCloud>
+  </div>
+</template>
--- src/js/alova.js
+++ src/js/alova.js
@@ -4,13 +4,12 @@ import { VueOptionsHook } from '@alova/vue-options';
 import store from '../store/index'
 
 export const alove = createAlova({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   statesHook: VueOptionsHook,
   requestAdapter: GlobalFetch(),
   beforeRequest(method) {
     // Suppose we need to add token to the request header
-    method.config.headers.post['Content-Type'] = 'application/json';
-    method.config.headers.put['Content-Type']  = 'application/json';
-    if (store.getters.token()) method.config.params['token'] = store.getters.token();
+    if (['post', 'put'].includes(method.type)) method.config.headers['Content-Type'] = 'application/json';
+    if (store.getters['token/token']()) method.config.params['token'] = store.getters['token/token']();
   }
 });
