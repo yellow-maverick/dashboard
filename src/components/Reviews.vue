034c4c310a4f31395e0701888ff2b40625a5dcf7
Hugo Melo <hugo@yellowmaverick.com>
2024-05-15 19:21:39 -0300

    Reviews: add pagination

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -19,6 +19,10 @@ export default{
       let r        = await alove.Get(`/v1/reviews`, { params: { ...this.filter, limit: this.perPage, page: this.page } })
       this.reviews = await r.clone().json()
     },
+    paginate (page) {
+      this.page = page
+      this.load()
+    }
   },
   mounted () {
     this.load()
@@ -32,9 +36,32 @@ export default{
 
 <template>
   <div>
-    <h6 class="ps-3">{{ $t('reviews.title') }}</h6>
+    <div class="d-flex justify-content-between">
+      <h6 class="ps-3">{{ $t('reviews.title') }}</h6>
+      <strong class="">{{ $t('reviews.page', { page: page }) }}</strong>
+    </div>
     <template v-if='filter' >
       <review v-for='review in reviews' :key='review.id' :review='review' />
+      <nav aria-label="Review navigation">
+        <ul class="pagination justify-content-center">
+          <li class="page-item">
+            <a class="page-link" href="#" :aria-label="$t('reviews.previous')" @click='paginate(page - 1)'>
+              <span aria-hidden="true">&laquo;</span>
+              <span class="sr-only">$t('reviews.previous')</span>
+            </a>
+          </li>
+          <template v-for='i in [-1,0,1]' :key=i>
+            <li class="page-item" :class='{ active: i == 0}' v-if='page + i > 0'><a class="page-link" href="#" @click='paginate(page + i)'>{{ page + i }}</a></li>
+          </template>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next" @click='paginate(page + 1)'>
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">$t('reviews.next')</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
     </template>
   </div>
 </template>
