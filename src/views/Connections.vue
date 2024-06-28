4c265d328698da13d25ed7ce2631f805585067b6
Braulio Oliveira <braulio@yellowmaverick.com>
2024-06-28 13:43:43 -0300

    Connections: show properties' connections

--- src/views/Connections.vue
+++ src/views/Connections.vue
@@ -37,23 +37,36 @@ export default {
 <template>
   <div class='container-fluid'>
     <div class='row'>
-      <div v-for='p in properties' class='card'>
-        <h5 class='card-title'>{{p.name}}</h5>
-        <h6 class='card-subtitle mb-2 text-muted'>Products</h6>
-
+      <div v-for='(p,i) in properties' class=card >
         <div id=accordion >
-          <div v-for='(pd,i) in p.products' class=accordion-item >
-            <h2 class=accordion-header >
-               <button class='btn btn-link' data-bs-toggle=collapse :data-bs-target='`#collapse${i}`' aria-expanded=true aria-controls=collapseOne >
-                 {{pd.name}}
-                 <img v-for='c in pd.connections' width='32px' class='mr-3' :src="sourceImage(c.source.slug)" >
-               </button>
-             </h2>
-              <div :id='`collapse${i}`' class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-               <div class=card-body >
-                 ...
-               </div>
-             </div>
+          <div class=accordion-item >
+            <h5 class=accordion-header >
+              <button class='btn btn-link' data-bs-toggle=collapse :data-bs-target='`#collapse-${i}`' aria-expanded=true aria-controls=collapseOne >
+                {{p.name}}
+                <img v-for='c in p.connections' width='20px' class='mr-3' :src="sourceImage(c.source.slug)" >
+              </button>
+            </h5>
+            <div :id='`collapse-${i}`' class=collapse aria-labelledby=headingOne data-parent=#accordion >
+              <div class=card-body >
+                ...
+              </div>
+            </div>
+          </div>
+
+          <h6 class='card-subtitle mb-2 text-muted'>Products</h6>
+
+          <div v-for='(pd,j) in p.products' class=accordion-item >
+            <h6 class=accordion-header >
+              <button class='btn btn-link' data-bs-toggle=collapse :data-bs-target='`#collapse-${i}-${j}`' aria-expanded=true aria-controls=collapseOne >
+                {{pd.name}}
+                <img v-for='c in pd.connections' width='20px' class='mr-3' :src="sourceImage(c.source.slug)" >
+              </button>
+            </h6>
+            <div :id='`collapse-${i}-${j}`' class=collapse aria-labelledby=headingOne data-parent=#accordion >
+              <div class=card-body >
+                ...
+              </div>
+            </div>
           </div>
         </div>
 
