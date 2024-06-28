2b9b36ceef06bacb2ee45ba1fe88b0e713262dfa
Braulio Oliveira <braulio@yellowmaverick.com>
2024-06-28 16:56:14 -0300

    Connections: align sources to the right

--- src/views/Connections.vue
+++ src/views/Connections.vue
@@ -38,38 +38,42 @@ export default {
   <div class='container-fluid'>
     <div class='row'>
       <div v-for='(p,i) in properties' class=card >
-        <div id=accordion >
+        <div id=property-accordion class=accordion >
           <div class=accordion-item >
             <h5 class=accordion-header >
-              <button class='btn btn-link' data-bs-toggle=collapse :data-bs-target='`#collapse-${i}`' aria-expanded=true aria-controls=collapseOne >
+              <button class=accordion-button data-bs-toggle=collapse :data-bs-target='`#collapse-${i}`' aria-expanded=true :aria-controls='`#collapse-${i}`'  >
                 {{p.name}}
-                <img v-for='c in p.connections' width='20px' class='mr-3' :src="sourceImage(c.source.slug)" >
+                <div class='d-flex justify-content-end' style='flex: auto' >
+                  <img class='mr-3' v-for='c in p.connections' width='20px' :src="sourceImage(c.source.slug)" >
+                </div>
               </button>
             </h5>
-            <div :id='`collapse-${i}`' class=collapse aria-labelledby=headingOne data-parent=#accordion >
-              <div class=card-body >
-                ...
+            <div :id='`collapse-${i}`' class='collapse show' aria-labelledby=headingOne data-parent=#property-accordion >
+
+              <h6 class='card-subtitle mb-2 text-muted'>Products</h6>
+
+              <div :id='`property-${i}-accordion`' class=accordion >
+                <div v-for='(pd,j) in p.products' class=accordion-item >
+                  <h6 class=accordion-header >
+                    <button class=accordion-button data-bs-toggle=collapse :data-bs-target='`#collapse-${i}-${j}`' aria-expanded=true :aria-controls='`#collapse-${i}-${j}`'  >
+                      {{pd.name}}
+                      <div class='d-flex justify-content-end' style='flex: auto' >
+                        <img v-for='c in pd.connections' width='20px' class='mr-3' :src="sourceImage(c.source.slug)" >
+                      </div>
+                    </button>
+                  </h6>
+                  <div :id='`collapse-${i}-${j}`' class=collapse aria-labelledby=headingOne :data-parent='`#property-${i}-accordion`' >
+                    <div class=card-body >
+                      ...
+                    </div>
+                  </div>
+                </div>
               </div>
+
             </div>
           </div>
 
-          <h6 class='card-subtitle mb-2 text-muted'>Products</h6>
-
-          <div v-for='(pd,j) in p.products' class=accordion-item >
-            <h6 class=accordion-header >
-              <button class='btn btn-link' data-bs-toggle=collapse :data-bs-target='`#collapse-${i}-${j}`' aria-expanded=true aria-controls=collapseOne >
-                {{pd.name}}
-                <img v-for='c in pd.connections' width='20px' class='mr-3' :src="sourceImage(c.source.slug)" >
-              </button>
-            </h6>
-            <div :id='`collapse-${i}-${j}`' class=collapse aria-labelledby=headingOne data-parent=#accordion >
-              <div class=card-body >
-                ...
-              </div>
-            </div>
-          </div>
         </div>
-
       </div>
     </div>
   </div>
