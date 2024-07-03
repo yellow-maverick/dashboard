136d4fde02e8332c1bc7fc241cc3ed9e4de14833
Hugo Melo <hugo@yellowmaverick.com>
2024-07-02 21:35:27 -0300

    Dashboard: cleanup

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,63 +1,19 @@
 <script>
-import Card from "@/examples/Cards/Card.vue";
+import Kpi from "@/components/Kpi.vue";
 import TagCloud from "@/components/TagCloud.vue";
-import GradientLineChart from "@/components/MixedChart.vue";
-
-import US from "@/assets/img/icons/flags/US.png";
-import DE from "@/assets/img/icons/flags/DE.png";
-import GB from "@/assets/img/icons/flags/GB.png";
-import BR from "@/assets/img/icons/flags/BR.png";
+import MixedChart from "@/components/MixedChart.vue";
 
 export default {
   name: "dashboard",
   data() {
     return {
-      stats: {
-        money: {
-          title: "Today's Money",
-          value: "$53,000",
-          percentage: "+55%",
-          iconClass: "fa-solid fa-star",
-          detail: "since yesterday",
-          iconBackground: "bg-gradient-primary",
-        },
-      },
-      sales: {
-        us: {
-          country: "United States",
-          sales: 2500,
-          value: "$230,900",
-          bounce: "29.9%",
-          flag: US,
-        },
-        germany: {
-          country: "Germany",
-          sales: "3.900",
-          value: "$440,000",
-          bounce: "40.22%",
-          flag: DE,
-        },
-        britain: {
-          country: "Great Britain",
-          sales: "1.400",
-          value: "$190,700",
-          bounce: "23.44%",
-          flag: GB,
-        },
-        brasil: {
-          country: "Brasil",
-          sales: "562",
-          value: "$143,960",
-          bounce: "32.14%",
-          flag: BR,
-        },
-      },
+      filter: null
     };
   },
   components: {
-    Card,
+    Kpi,
     TagCloud,
-    GradientLineChart,
+    MixedChart,
   },
 };
 </script>
@@ -65,27 +21,24 @@ export default {
   <div class="container-fluid">
     <div class="row">
       <div class="col-lg-12">
-        <div class="row">
-          <div class="col-lg-3 col-md-6 col-12">
-            <card
-              :title="stats.money.title"
-              :value="stats.money.value"
-              :percentage="stats.money.percentage"
-              :iconClass="stats.money.iconClass"
-              :iconBackground="stats.money.iconBackground"
-              :detail="stats.money.detail"
-              directionReverse
-            ></card>
+        <div class="row mt-4">
+          <div class="offset-2 col-lg-3 col-md-6 col-12">
+            <Kpi :filter="filter" trend="yoy" segment="reviews_count" directionReverse v-if='filter' ></Kpi>
+          </div>
+          <div class="offset-2 col-lg-3 col-md-6 col-12">
+            <Kpi :filter="filter" trend="yoy" segment="numerical" directionReverse v-if='filter' ></Kpi>
           </div>
         </div>
+
         <div class="row">
           <div class="col-lg-12 mb-lg">
             <!-- line chart -->
             <div class="card z-index-2">
-              <gradient-line-chart />
+              <mixed-chart title='reviews-ratings' :filter='filter' v-if='filter' />
             </div>
           </div>
         </div>
+
         <div class="row mt-4">
           <div class="col-lg-12 mb-4">
             <div class="card">
