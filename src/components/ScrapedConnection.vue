7fde7f70f9310171dcf1bfac44be5b8655a84656
Braulio Oliveira <braulio@yellowmaverick.com>
2024-08-28 16:34:10 -0300

    AddProduct: refactor scraping into the ScrapedConnection component

---
+++ src/components/ScrapedConnection.vue
@@ -0,0 +1,83 @@
+<script>
+import { alova } from '../js/alova.js'
+import PhotoGallery from '@/components/PhotoCardGallery.vue'
+
+export default {
+  data() {
+    return {
+      scraping: false,
+    }
+  },
+  props: ['url', 'scraped'],
+
+  components: {PhotoGallery},
+
+  watch: {
+    url() {
+      this.scrape()
+    }
+  },
+
+  methods: {
+    async scrape() {
+      this.scraping = true
+      let r = (await (await alova.Get(`/v1/connections/scrape`, {params: {url: this.url}})).clone().json())
+      this.scraped.error        = r.status
+      this.scraped.source       = r.source
+      this.scraped.name         = r.property_info.name
+      this.scraped.currency     = r.property_info.price_info.currency || r.schema_org?.offer?.priceCurrency
+      this.scraped.price        = r.property_info.price_info.price    || r.schema_org?.offer?.price
+      this.scraped.availability = r.property_info.availability        || r.schema_org?.offer?.availability == 'InStock'
+      this.scraped.images       = r.property_info.images
+      this.scraping = false
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <div :class='{"spinner-border": scraping}' >
+    </div>
+    <span v-if=scraping > {{$t('connections.scraping')}} </span>
+    <div v-if=scraped.error >
+      <span > {{$t('connections.scraping_error')}} </span>
+    </div>
+
+    <div class='card col-6 mb-3' v-if=scraped.source >
+      <div class=card-body >
+
+        <PhotoGallery :images=scraped.images />
+
+        <h3 class='text-primary pb-3 border-bottom' v-if=scraped.price >
+          <span class=price > {{scraped.price}} </span>
+          <span class=currency > &nbsp;{{scraped.currency}} </span>
+        </h3 >
+
+        <div class='form-group row border-bottom' >
+          <label class='col-5 col-form-label' > {{ $t('products.available.title') }} </label>
+          <div class='col-6' >
+            <p class=form-control-plaintext >
+            {{$t(`products.available.${scraped.availability ? 'yes' : 'no'}`)}}
+            </p>
+          </div>
+        </div>
+
+          <div class='form-group row border-bottom' >
+            <label class='col-5 col-form-label' > {{ $t('connections.site') }} </label>
+            <div class='col-6' >
+              <p class=form-control-plaintext > {{scraped.source.name}} </p>
+            </div>
+          </div>
+
+            <div class='form-group row border-bottom' >
+              <label class='col-5 col-form-label' > {{ $t('connections.name') }} </label>
+              <div class='col-6' >
+                <a class=form-control-plaintext :href=url > {{scraped.name}} </a>
+              </div>
+            </div>
+      </div>
+    </div>
+  </div>
+</template>
+
--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -2,13 +2,12 @@
 import { alova } from '../js/alova.js'
 //import _ from 'lodash'
 import ProductForm from '@/components/ProductForm.vue'
-import PhotoGallery from '@/components/PhotoCardGallery.vue'
+import ScrapedConnection from '@/components/ScrapedConnection.vue'
 
 export default {
   data() {
     return {
       property_id: this.$route.query.property_id,
-      scraping: false,
       product: {
         connection: {
           source: null,
@@ -16,45 +15,32 @@ export default {
         },
         name: null,
       },
-      scraped: {
-        error:        null,
-        name:         null,
-        source:       null,
-        currency:     null,
-        price:        null,
-        availability: null,
-      },
+      scraped: { },
       categories: [],
     }
   },
   props: [],
-  components: {ProductForm, PhotoGallery},
+  components: {ProductForm, ScrapedConnection},
 
   created() {
     this.load()
   },
 
+  watch: {
+    scraped: {
+      handler() {
+        this.product.name                = this.scraped.name
+        this.product.connection.name     = this.scraped.name
+        this.product.connection.price    = this.scraped.price
+        this.product.connection.currency = this.scraped.currency
+        this.product.connection.source   = this.scraped.source.slug
+        this.product.connection.images   = this.scraped.images
+      },
+      deep: true
+    }
+  },
+
   methods: {
-
-    async scrape() {
-      if (!this.product.connection.url) return
-      this.scraping = true
-      let r = (await (await alova.Get(`/v1/connections/scrape`, {params: {url: this.product.connection.url}})).clone().json())
-      this.scraped.error        = r.status
-      this.scraped.name         = r.property_info.name
-      this.scraped.currency     = r.property_info.price_info.currency || r.schema_org?.offer?.priceCurrency
-      this.scraped.price        = r.property_info.price_info.price    || r.schema_org?.offer?.price
-      this.scraped.availability = r.property_info.availability        || r.schema_org?.offer?.availability == 'InStock'
-      this.scraped.source       = r.source
-
-      this.product.name = this.scraped.name
-      this.product.connection.price    = this.scraped.price
-      this.product.connection.currency = this.scraped.currency
-      this.product.connection.source   = r.source.slug
-      this.product.connection.images   = r.property_info.images
-
-      this.scraping = false
-    },
 
     async save() {
       let r = (await (await alova.Post(`/v1/products`, {product: this.product})).clone().json())
@@ -82,50 +68,10 @@ export default {
       <form>
         <div class=form-group >
           <label for=url >{{ $t('products.url') }}</label>
-          <input name=url v-model=product.connection.url type=text @input=scrape() class=form-control />
+          <input name=url v-model=product.connection.url type=text class=form-control />
         </div>
 
-        <div :class='{"spinner-border": scraping}' >
-        </div>
-        <span v-if=scraping > {{$t('products.scraping')}} </span>
-        <div v-if=scraped.error >
-          <span > {{$t('products.scraping_error')}} </span>
-        </div>
-
-        <div class='card col-6 mb-3' v-if=scraped.source >
-          <div class=card-body >
-
-            <PhotoGallery :images=product.connection.images />
-
-            <h3 class='text-primary pb-3 border-bottom' v-if=scraped.price >
-              <span class=price > {{scraped.price}} </span>
-              <span class=currency > &nbsp;{{scraped.currency}} </span>
-            </h3 >
-
-            <div class='form-group row border-bottom' >
-              <label class='col-5 col-form-label' > {{ $t('products.available.title') }} </label>
-              <div class='col-6' >
-                <p class=form-control-plaintext >
-                  {{$t(`products.available.${scraped.availability ? 'yes' : 'no'}`)}}
-                </p>
-              </div>
-            </div>
-
-            <div class='form-group row border-bottom' >
-              <label class='col-5 col-form-label' > {{ $t('products.site') }} </label>
-              <div class='col-6' >
-                <p class=form-control-plaintext > {{scraped.source.name}} </p>
-              </div>
-            </div>
-
-            <div class='form-group row border-bottom' >
-              <label class='col-5 col-form-label' > {{ $t('products.name') }} </label>
-              <div class='col-6' >
-                <a class=form-control-plaintext :href=product.connection.url > {{scraped.name}} </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ScrapedConnection :scraped=scraped :url=product.connection.url />
 
         <h5 role=button data-bs-toggle=collapse data-bs-target=#more-details aria-expanded=true aria-controls=more-details >
           {{ $t('products.more_details') }}
